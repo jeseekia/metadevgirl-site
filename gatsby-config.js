20070a60693e0f042f0fb6a2ede1ff9a1484e378
Jeseekia <jeseekia@gmail.com>
2020-03-27 13:59:54 -0400

    Remove the starter metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MetaDevGirl: Crawl, Walk, Run`,
+    description: `Get exposure to software topics and learn how to get up and running.`,
+    author: `@metadevgirl`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
